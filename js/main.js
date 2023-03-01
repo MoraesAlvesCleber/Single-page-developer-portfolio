809cf4250bd1bb86abd2c5b459c998240bd14c2a
Cleber Moraes <alvesmoraesb@gmail.com>
2023-03-01 13:01:28 -0300

    Link e Imagem
    Escurecendo imagem

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 const divImgs = document.querySelectorAll('.div-img');
+ 
 
 
 
 divImgs.forEach((divImg) => {
 
   const divLink = divImg.querySelector('.div-link');
+  const img = divImg.querySelector('.projects-img');
 
   divImg.addEventListener('mouseover', () => {
     divLink.style.display = 'flex';
+    img.style.opacity = '0.8';
+    img.style.filter = 'brightness(0.2)';
+    img.style.transition = 'opacity 1s ease';
   
 
   });
@@ -15,6 +20,9 @@ divImgs.forEach((divImg) => {
   // Adiciona um event listener para o evento "mouseout"
   divImg.addEventListener('mouseout', () => {
     divLink.style.display = 'none';
+    img.style.opacity = '1';
+    img.style.filter = 'brightness(1)';
+    img.style.transition = 'opacity 1s ease';
 
   });
 });
