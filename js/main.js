9540c969efd9c486bfae490ae21c6232578d5705
Cleber Moraes <alvesmoraesb@gmail.com>
2023-02-28 21:50:36 -0300

    Inserindo link nas imagens

--- js/main.js
+++ js/main.js
@@ -1,45 +1,20 @@
-// Seleciona todas as imagens com a classe "projects-img"
-const images = document.querySelectorAll('.div-link');
- 
+const divImgs = document.querySelectorAll('.div-img');
 
-// Para cada imagem, adiciona um event listener para o evento "mouseover"
-images.forEach((image) => {
-  image.addEventListener('mouseover', () => {
-    // Define a opacidade da imagem como 50%
-    
 
-    // Cria os links
-    const link1 = document.createElement('a');
-    const link2 = document.createElement('a');
-    const br = document.createElement('br');
 
-    
+divImgs.forEach((divImg) => {
 
-    // Define o endereço dos links
-    link1.href = '#';
-    link2.href = '#';
+  const divLink = divImg.querySelector('.div-link');
 
-    // Define o texto dos links
-    link1.textContent = 'View project';
-    link2.textContent = 'View code';
+  divImg.addEventListener('mouseover', () => {
+    divLink.style.display = 'flex';
+  
 
-    // Adiciona os links dentro da imagem
-    image.appendChild(link1);
-    image.appendChild(link2);
-
-    link1.style.textDecoration = 'none';
-    link2.style.textDecoration = 'none';
-    
-    link2.style.marginTop = '40px';
   });
 
   // Adiciona um event listener para o evento "mouseout"
-  image.addEventListener('mouseout', () => {
-    // Define a opacidade da imagem como 100%
-    image.style.opacity = 1;
+  divImg.addEventListener('mouseout', () => {
+    divLink.style.display = 'none';
 
-    // Remove os links da imagem
-    image.removeChild(image.lastChild);
-    image.removeChild(image.lastChild);
   });
 });
